ce939340c590a04315ed2f547035c8faa5539515
dlwjiang <dlwjiang@gmail.com>
2023-11-04 13:12:44 -0700

    init basic

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,110 +1,74 @@
 import Image from "next/image";
-import { Quicksand } from "next/font/google";
+import { Fredoka, Ma_Shan_Zheng, ZCOOL_KuaiLe } from "next/font/google";
+import content from "./content";
 
-const font = Quicksand({ weight: "600", subsets: [] });
-const accentColor = "#b70943";
+const font = Fredoka({ weight: "400", subsets: [] });
+const chineseFont = ZCOOL_KuaiLe({ weight: "400", subsets: [] });
 
 export default function Home() {
   return (
     <main
-      className={`${font.className} grid sm:grid-cols-1 md:grid-cols-2`}
+      className={`${font.className} h-screen overflow-y-scroll overflow-x-hidden bg-amber-50`}
       style={{ perspective: "10px" }}
     >
-      <div className="col-span-1 h-screen flex flex-col justify-center items-center background-gradient">
-        <div className="md:mt-10 text-white">
-          <div
-            className="text-3xl text-center tracking-wide"
-            style={{
-              textShadow: `2px 2px ${accentColor}`,
-            }}
-          >
-            しゃぶしゃぶ
-          </div>
-          <div
-            className="text-5xl text-center tracking-wide mb-1"
-            style={{
-              textShadow: `3px 3px ${accentColor}`,
-            }}
-          >
-            Shabu Shabu
-          </div>
-          <div
-            className=" text-2xl text-center tracking-wide"
-            style={{
-              textShadow: `2px 2px ${accentColor}`,
-            }}
-          >
-            Full Pot Development
-          </div>
-          <Image
-            src="/shabuPot.png"
-            alt="Shabut Pot"
-            width={400}
-            height={400}
-            priority
-          />
+      <header
+        className="relative flex flex-col items-center justify-center h-full"
+        style={{ transformStyle: "preserve-3d", zIndex: -1 }}
+      >
+        <Image
+          src="/sun.png"
+          alt="Sun"
+          className="h-full w-full absolute object-cover"
+          style={{ transform: "translateZ(-10px) scale(2)" }}
+          width={1200}
+          height={400}
+          priority
+        />
+        <Image
+          src="/background.png"
+          alt="Fun Background"
+          className="h-full w-full absolute object-cover opacity-80"
+          style={{ transform: "translateZ(-5px) scale(1.5)" }}
+          width={1200}
+          height={400}
+          priority
+        />
+        <Image
+          src="/foreground.png"
+          alt="Mountainous Foreground"
+          className="h-full w-full absolute object-cover"
+          width={1200}
+          height={400}
+          priority
+        />
+        <div
+          className="text-white text-6xl z-10 text-center px-16"
+          style={{
+            textShadow: "2px 2px 2px #027780",
+            transform: "translateZ(2px) scale(0.8)",
+          }}
+        >
+          A Casual Dao De Jing
         </div>
-      </div>
-      <div className="col-span-1 h-screen bg-white flex flex-col justify-center items-center px-12">
-        <div className={`flex flex-col items-center justify-center`}>
-          <div className="p-4 bg-white">
-            Shabu Shabu is a "Full Pot" development template that scaffolds an
-            application using modern tools optimized for rapid prototyping, side
-            projects, and MVP iteration. It uses{" "}
-            <a
-              href="https://nextjs.org/"
-              target="_blank"
-              className="link-gradient"
+      </header>
+      <section className="md:px-48">
+        {content.map((e, i) => (
+          <div
+            key={e.title}
+            className="bg-white p-2 m-2 md:p-8 md:m-8 rounded-xl shadow whitespace-pre-wrap bg-white/70"
+          >
+            <div className="text-yellow-600 text-2xl">{e.title}</div>
+            <div
+              className={`${chineseFont.className} text-sm md:text-base text-stone-600/50 tracking-wide`}
             >
-              Next.js
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://www.convex.dev/"
-              target="_blank"
-              className="link-gradient"
-            >
-              Convex
-            </a>{" "}
-            as the core technologies and also sets up boilerplate for useful
-            tooling such as ESLint, Typescript,{" "}
-            <a
-              href="https://tailwindcss.com/"
-              target="_blank"
-              className="link-gradient"
-            >
-              Tailwind
-            </a>
-            , and{" "}
-            <a
-              href="https://ui.shadcn.com/"
-              target="_blank"
-              className="link-gradient"
-            >
-              Prettier.
-            </a>
+              {e.original}
+            </div>
+            <div className="leading-7 text-sm md:text-base tracking-wide text-stone-700">
+              {e.english}
+            </div>
           </div>
-          <div className="p-4 bg-white mb-8">
-            It is inspired by{" "}
-            <a
-              href="https://prettier.io/"
-              target="_blank"
-              className="link-gradient"
-            >
-              shadcn's
-            </a>{" "}
-            philosophy of just giving you code rather than dependencies and
-            documentation. It avoids the over-processing of too many moving
-            pieces to emphasize a few high quality raw ingredients. Just fork
-            the template and start developing.
-          </div>
-          <div className="text-white rounded shadow px-8 py-4 tracking-wide cursor-pointer button-gradient active:bg-red-700">
-            <a href="https://github.com/Theory-A/ShabuShabu" target="_blank">
-              Get the Template
-            </a>
-          </div>
-        </div>
-      </div>
+        ))}
+      </section>
     </main>
   );
 }
